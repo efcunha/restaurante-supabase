a31aef39e40d570a1c5ecd40e59f6d8a5d90bf73
Edson Fernandes Cunha <lucianamachado28@hotmail.com>
2026-02-06 19:53:48 -0300

    Remove an unspecified file.

--- restaurante-app/scripts/fix_pagamentos_constraint.sql
+++
@@ -1,5 +0,0 @@
--- Drop the restrictive check constraint on payment_method
--- This allows any payment method value to be stored
-ALTER TABLE IF EXISTS public.pagamentos 
-DROP CONSTRAINT IF EXISTS pagamentos_payment_method_check;
-
