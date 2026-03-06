5af2938f0b7065b9bcf4675ee5f1123056708b7c
Edson Fernandes Cunha <lucianamachado28@hotmail.com>
2026-03-06 07:08:28 -0300

    feat: Inicializa o projeto restaurante-web com arquivos package.json e package-lock.json.

--- restaurante-web/cypress/e2e/cloud_test.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Cloud Trial Connection Test', () => {
-  it('successfully connects to the app', () => {
-    cy.visit('/')
-    cy.title({ timeout: 30000 }).should('include', 'Restaurante Web')
-  })
-})
