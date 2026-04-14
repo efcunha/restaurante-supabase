9257ca2501498dc09b0ff1ad42e184709417c661
Edson Fernandes Cunha <lucianamachado28@hotmail.com>
2026-04-14 17:24:50 -0300

    feat: update Card stories to use new component path and improve type definitions

--- restaurante-web/.storybook/main.ts
+++ restaurante-web/.storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   stories: [
     '../src/ui/**/*.stories.@(js|jsx|mjs|ts|tsx)',
     '../src/features/admin/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../src/screens/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-docs',
