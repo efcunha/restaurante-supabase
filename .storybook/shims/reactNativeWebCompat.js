279e84e870f05718157bfae7517012ee717f79b1
Edson Fernandes Cunha <lucianamachado28@hotmail.com>
2026-04-20 07:10:10 -0300

    feat: enhance Storybook shims with network info and asset resolution support

--- .storybook/shims/reactNativeWebCompat.js
+++ .storybook/shims/reactNativeWebCompat.js
@@ -1,5 +1,23 @@
 import * as ReactNativeWeb from 'react-native-web';
 
+const noop = () => undefined;
+
+const rncNetInfoModule = {
+  configure: noop,
+  addListener: noop,
+  removeListeners: noop,
+  getCurrentState() {
+    return Promise.resolve({
+      type: 'wifi',
+      isConnected: true,
+      isInternetReachable: true,
+      details: {
+        isConnectionExpensive: false,
+      },
+    });
+  },
+};
+
 export const TurboModuleRegistry = {
   get() {
     return null;
@@ -9,9 +27,33 @@ export const TurboModuleRegistry = {
   },
 };
 
+export class NativeEventEmitter {
+  addListener() {
+    return { remove: noop };
+  }
+
+  removeAllListeners() {
+    return;
+  }
+
+  removeListener() {
+    return;
+  }
+
+  emit() {
+    return false;
+  }
+}
+
+export const NativeModules = {
+  RNCNetInfo: rncNetInfoModule,
+};
+
 export * from 'react-native-web';
 
 export default {
   ...ReactNativeWeb,
   TurboModuleRegistry,
+  NativeEventEmitter,
+  NativeModules,
 };
